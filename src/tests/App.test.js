f1e7608cac5124fa3ccc102d1dae74fb6a586473
jiyoonZ <wldbsl416@gmail.com>
2022-12-28 09:20:29 +0900

    Feat: 간단한 테스트코드 작성연습(TDD)

---
+++ src/tests/App.test.js
@@ -0,0 +1,8 @@
+import {render, screen} from "@testing-library/react";
+import App from "../App";
+
+it("로그인 화면 렌더링", () => {
+  render(<App />);
+  const linkElement = screen.getByText("로그인");
+  expect(linkElement).toBeInTheDocument();
+});
